7a5a3503a11d494a920f08058f7a85cc88c39c87
Koshi Ishide <root@DESKTOP-TCEGJDK.localdomain>
2020-10-09 11:58:02 +0900

    finish

--- app/templatetags/test1.py
+++ app/templatetags/test1.py
@@ -7,15 +7,15 @@ register = template.Library()
 #0,1,2
 @register.simple_tag
 def test2(a):
-    return 0
+    return a
 
 
 @register.simple_tag
+@register.filter 
 def test(a):
     dict = {1:0,2:0}
     list = a
     for k in list:
-        print(k)
         if (int(k.type_name) >1):
             dict[1]+=1
             dict[2]+=1
@@ -24,7 +24,6 @@ def test(a):
         else:
             continue
     if (((dict[1])>=2) and (dict[2]>=2)):
-        return 1
-    else:
         return 0
-    return 0
+    else:
+        return 1
